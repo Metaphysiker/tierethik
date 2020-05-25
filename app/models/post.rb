18b03404691ef7c71b566e8ba78f968f1a31c21c
Metaphysiker <s.raess@me.com>
2020-05-25 17:43:25 +0200

    completed generator

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
+before_save :update_search_field
+scope :posts_ilike, ->(search_term) { where("search_field ILIKE ?", search_term) }
+
+def update_search_field
+  self.search_field =
+[
+ ""
+].compact.join(' ')
+end
 extend FriendlyId
 friendly_id :title, use: :slugged
 before_save :update_search_field
