e2aa1d6c565051da883ff54c77fc5c122aee36df
Metaphysiker <s.raess@me.com>
2020-05-26 20:25:53 +0200

    nicer index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
   end
 
   def is_current_user_admin?
-    !current_user.nil? && current_user.roles.where(name: "admin").exists?
+    !current_user.nil? && current_user.admin?
   end
 
 end
