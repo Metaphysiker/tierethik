22ac05061674dfcede7f1a084e3b8a93be3efbbf
Metaphysiker <s.raess@me.com>
2020-06-08 12:25:53 +0200

    made it work

--- app/javascript/packs/marker.js
+++ app/javascript/packs/marker.js
@@ -8,87 +8,92 @@ import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import TileJSON from 'ol/source/TileJSON';
 import VectorSource from 'ol/source/Vector';
 import {Icon, Style} from 'ol/style';
+import OSM from 'ol/source/OSM';
+import {useGeographic} from 'ol/proj';
+
+useGeographic();
+
+$( document ).ready(function() {
+  var iconFeature = new Feature({
+    geometry: new Point([8.22, 46.81]),
+    name: 'Null Island',
+    population: 4000,
+    rainfall: 500
+  });
 
 
-var iconFeature = new Feature({
-  geometry: new Point([8.32, 46.67]),
-  name: 'Null Island',
-  population: 4000,
-  rainfall: 500
-});
 
-var iconStyle = new Style({
-  image: new Icon({
-    anchor: [0.5, 46],
-    anchorXUnits: 'fraction',
-    anchorYUnits: 'pixels',
-    src: 'data/icon.png'
-  })
-});
+  var iconStyle = new Style({
+    image: new Icon({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'pixels',
+      src: "https://picsum.photos/id/237/20/30"
+      //src: "<%= image_tag(\"pin\")%>"
+    })
+  });
 
-iconFeature.setStyle(iconStyle);
+  iconFeature.setStyle(iconStyle);
 
-var vectorSource = new VectorSource({
-  features: [iconFeature]
-});
+  var vectorSource = new VectorSource({
+    features: [iconFeature]
+  });
 
-var vectorLayer = new VectorLayer({
-  source: vectorSource
-});
+  var vectorLayer = new VectorLayer({
+    source: vectorSource
+  });
 
-var rasterLayer = new TileLayer({
-  source: new TileJSON({
-    url: 'https://a.tiles.mapbox.com/v3/aj.1x1-degrees.json',
-    crossOrigin: ''
-  })
-});
+  var rasterLayer = new TileLayer({
+    source: new TileJSON({
+      url: 'https://a.tiles.mapbox.com/v3/aj.1x1-degrees.json',
+      crossOrigin: ''
+    })
+  });
 
-var map = new Map({
-  layers: [rasterLayer, vectorLayer],
-  target: document.getElementById('map'),
-  view: new View({
-    center: [0, 0],
-    zoom: 3
-  })
-});
+  var osm =  new TileLayer({
+      source: new OSM()
+    })
 
-var element = document.getElementById('popup');
 
-var popup = new Overlay({
-  element: element,
-  positioning: 'bottom-center',
-  stopEvent: false,
-  offset: [0, -50]
-});
-map.addOverlay(popup);
 
-// display popup on click
-map.on('click', function(evt) {
-  var feature = map.forEachFeatureAtPixel(evt.pixel,
-    function(feature) {
-      return feature;
-    });
-  if (feature) {
-    var coordinates = feature.getGeometry().getCoordinates();
-    popup.setPosition(coordinates);
-    $(element).popover({
-      placement: 'top',
-      html: true,
-      content: feature.get('name')
-    });
-    $(element).popover('show');
-  } else {
-    $(element).popover('destroy');
-  }
-});
+  var element = document.getElementById('popup');
 
-// change mouse cursor when over marker
-map.on('pointermove', function(e) {
-  if (e.dragging) {
-    $(element).popover('destroy');
-    return;
-  }
-  var pixel = map.getEventPixel(e.originalEvent);
-  var hit = map.hasFeatureAtPixel(pixel);
-  map.getTarget().style.cursor = hit ? 'pointer' : '';
+  var popup = new Overlay({
+    element: element,
+    positioning: 'bottom-center',
+    stopEvent: false,
+    offset: [0, -50]
+  });
+  map.addOverlay(popup);
+
+  // display popup on click
+  map.on('click', function(evt) {
+    var feature = map.forEachFeatureAtPixel(evt.pixel,
+      function(feature) {
+        return feature;
+      });
+    if (feature) {
+      var coordinates = feature.getGeometry().getCoordinates();
+      popup.setPosition(coordinates);
+      $(element).popover({
+        placement: 'top',
+        html: true,
+        content: feature.get('name')
+      });
+      $(element).popover('show');
+    } else {
+      $(element).popover('hide');
+    }
+  });
+
+  // change mouse cursor when over marker
+  map.on('pointermove', function(e) {
+    if (e.dragging) {
+      $(element).popover('hide');
+      return;
+    }
+    var pixel = map.getEventPixel(e.originalEvent);
+    var hit = map.hasFeatureAtPixel(pixel);
+    map.getTarget().style.cursor = hit ? 'pointer' : '';
+  });
 });
