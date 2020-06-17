6d72b8d0887f8f093355f15595707faebf60f1c3
Metaphysiker <s.raess@me.com>
2020-06-17 19:45:44 +0200

    remove precompile option

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module Tierethik
 
     #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
