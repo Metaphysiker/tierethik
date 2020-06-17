89721ba9543bfb0dd69ab38154f63db9162e93e7
Metaphysiker <s.raess@me.com>
2020-06-17 20:46:41 +0200

    trying to add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("channels")
 //require("ol")
 //require("packs/marker")
 
+require("../stylesheets/application")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,3 +21,5 @@ require("channels")
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+//require("bootstrap")
