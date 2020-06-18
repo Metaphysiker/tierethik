fb5f13f6e7d1310293da6941931fa6243619aa30
Metaphysiker <s.raess@me.com>
2020-06-18 14:52:34 +0200

    minor

--- import-heroku-database.sh
+++ import-heroku-database.sh
@@ -1,7 +1,7 @@
 rm -f latest.dump
 heroku pg:backups:capture
 heroku pg:backups:download
-rails db:drop
+rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 rails db:create
 rails db:migrate
 pg_restore --verbose --clean --no-acl --no-owner -d tierethik_development latest.dump
